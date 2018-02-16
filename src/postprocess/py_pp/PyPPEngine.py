fe97b5fde38aeed2f2177606014358b8a2a301bd
Alexandre Proy <alexandre.proy@grenoble-inp.org>
2018-02-16 16:46:59 +0100

    py_pp_engine: updated PyPPEngine
    To accomodate with the changes made to CityManager, namely:
    - number of Neighborhoods
    - threshold of open umbrellas per Neighborhood

--- src/postprocess/py_pp/PyPPEngine.py
+++ src/postprocess/py_pp/PyPPEngine.py
@@ -13,9 +13,13 @@ import time
 if __name__ == "__main__":
 
     try:
-        TurinManager = CityManager("Turin", 45.106234, 7.619275, 45.024758, 7.719869)
+        # Creating a CityManager for turin with the coordinates of a NW point
+        # and of a SE point, it will be composed of 4*4 Neighborhood and the
+        # threshold of open umbrellas per neighborhood after which a rain
+        # warning is sent is set to 2
+        TurinManager = CityManager("Turin", 45.106234, 7.619275, 45.024758, 7.719869, 4, 2)
         TurinManager.manage()
-        TurinManager.myPyPPEMqttClient.mySubscribe("/city/notifications/#")
+        TurinManager.myPyPPEMqttClient.mySubscribe("/Turin/notifications/#")
 
         while True:
             time.sleep(1)
