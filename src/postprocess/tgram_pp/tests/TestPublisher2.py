12df7227f958dc08657d86638aa2a8c3c8e0850c
Alexandre Proy <alexandre.proy@grenoble-inp.org>
2018-02-25 00:36:09 +0100

    tgram_pp_engine: changed MQTT topic to respect Wiki
    And what we have decided

--- src/postprocess/tgram_pp/tests/TestPublisher2.py
+++ src/postprocess/tgram_pp/tests/TestPublisher2.py
@@ -12,25 +12,25 @@ if __name__ == "__main__":
         payload = {}
         if endInput == '1':
             payload = {"chat_ID": 1, "location": {"latitude": 45.03, "longitude": 7.62}, "status": "closed", "timestamp": str(timenow)}
-            test.mqtt_client.myPublish("/Turin/notifications/u1", json.dumps(payload))
+            test.mqtt_client.myPublish("/Turin/1/notifications", json.dumps(payload))
         elif endInput == '2':
             payload = {"chat_ID": 1, "location": {"latitude": 45.03, "longitude": 7.62}, "status": "open", "timestamp": str(timenow)}
-            test.mqtt_client.myPublish("/Turin/notifications/u1", json.dumps(payload))
+            test.mqtt_client.myPublish("/Turin/1/notifications", json.dumps(payload))
         elif endInput == '3':
             payload = {"chat_ID": 1, "location": {"latitude": 45.08, "longitude": 7.68}, "status": "open", "timestamp": str(timenow)}
-            test.mqtt_client.myPublish("/Turin/notifications/u1", json.dumps(payload))
+            test.mqtt_client.myPublish("/Turin/1/notifications", json.dumps(payload))
         elif endInput == '4':
             payload = {"chat_ID": 2, "location": {"latitude": 45.03, "longitude": 7.62}, "status": "open", "timestamp": str(timenow)}
-            test.mqtt_client.myPublish("/Turin/notifications/u2", json.dumps(payload))
+            test.mqtt_client.myPublish("/Turin/2/notifications", json.dumps(payload))
         elif endInput == '5':
             payload = {"chat_ID": 2, "location": {"latitude": 45.03, "longitude": 7.62}, "status": "closed", "timestamp": str(timenow)}
-            test.mqtt_client.myPublish("/Turin/notifications/u2", json.dumps(payload))
+            test.mqtt_client.myPublish("/Turin/2/notifications", json.dumps(payload))
         elif endInput == '6':
-            payload = {"chat_ID": 3, "location": {"latitude": 45.03, "longitude": 7.62}, "status": "closed", "timestamp": str(timenow)}
-            test.mqtt_client.myPublish("/Turin/notifications/u3", json.dumps(payload))
+            payload = {"chat_ID": 459592011, "location": {"latitude": 45.03, "longitude": 7.62}, "status": "closed", "timestamp": str(timenow)}
+            test.mqtt_client.myPublish("/Turin/459592011/notifications", json.dumps(payload))
         else:
             payload = {"invalid": True}
-            test.mqtt_client.myPublish("/Turin/notifications/", json.dumps(payload)) 
+            test.mqtt_client.myPublish("/Turin/0/notifications/", json.dumps(payload)) 
 
         print("Publishing: '%s'" % (payload))  
         endInput = input()
