1c35a326317232a5b8aa2a6c9131614c25285fdb
Alexandre Proy <alexandre.proy@grenoble-inp.org>
2018-02-25 23:16:49 +0100

    rainbot_catalog: registration of bot to catalog
    Using conf file.

--- src/interface/telegram_bot/launch_bot.py
+++ src/interface/telegram_bot/launch_bot.py
@@ -9,8 +9,29 @@ Ctrl+'C' is pressed.
 
 from bot.BotManager import BotManager
 import time
+import requests
+import json
+import os, sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), \
+                                            './../../catalog/')))
+from classes import IamAlive
 
 if __name__ == '__main__':
+
+    # Loading configuration file
+    conf = json.load(open("conf.json", "r"))
+
+    # Retrieving catalog URL to register to it
+    url = conf["catalog"]["URL"]
+    # Retrieving the payload expected by the catalog
+    payload = conf["catalog"]["expected_payload"]
+    # Retrieving the interval of time at which our actor should communicate
+    # with the catalog
+    interval = conf["catalog"]["interval"]
+
+    # Starting to send registration messages to catalog
+    IamAlive(url, payload, interval)
 
     try:
         myBotManager = BotManager()
